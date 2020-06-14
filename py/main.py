30b38c7ec141df778ed04c48b274bf6e19823f8c
jmoreau <34433023+jmoreau77@users.noreply.github.com>
2020-06-13 19:59:38 -0500

    Revert "Revert "Add daily reward command""
    This reverts commit 9167f1d2d79741e378a93e4f6f590603b3a7d244.

--- py/main.py
+++ py/main.py
@@ -10,6 +10,7 @@ import sqlite3
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
+from discord.ext.commands import CommandNotFound
 conn = sqlite3.connect("./db/userdata.db")
 c = conn.cursor()
 
@@ -26,7 +27,23 @@ async def on_ready():
 
 @bot.event
 async def on_guild_join(guild):
-    c.execute(f"INSERT INTO server VALUES ({guild.id},'.',1.00)")
+    c.execute(f"INSERT INTO server VALUES ({guild.id},{get_prefix(guild)} = '.',100.00")
     conn.commit()
+
+
+
+@bot.command(aliases=['setdaily'])
+async def set_daily_reward(ctx, *, amount: float):
+    daily_reward = format(amount, '.2f')
+    c.execute(f"UPDATE server SET daily = {daily_reward} WHERE id = {ctx.guild.id}")
+    conn.commit()
+    await ctx.send(f"Set daily reward to  ${daily_reward}");
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        return
+    raise error
 
 bot.run(TOKEN)
